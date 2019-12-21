ed042dfdcd1ddcefe1ebaab7f87919d74f7316a3
XBDDDBX <1025573340@qq.com>
2019-12-21 16:39:25 +0800

    HelloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	public static void main (String[] args){
+		System.out.println("HelloWorld");
+		System.out.println("HelloLi");
+		System.out.println("HelloYi");
+		System.out.println("HelloMing");
+	}
+}
